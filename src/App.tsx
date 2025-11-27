5207e2f98636f1dc31e9eb592445db073b89df49
Seth Hoong <hoongseth@gmail.com>
2025-11-27 14:12:37 -0500

    Add choice list

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,23 @@ function BodyText({text}: {text: string}) {
   </div>
 }
 
+type Choice = {
+  content: string,
+  next: string,
+}
+
+function ChoiceList({choices}: {choices: Choice[]}) {
+  const choiceList = choices.map((choice, i) => <button className="btn-choice" key={i}>{choice.content}</button>);
+
+  return choiceList;
+}
+
 export default function App() {
   return (
     <>
       <Topbar />
       <BodyText text={game.content.start.content} />
+      <ChoiceList choices={game.content.start.choices} />
     </>
   );
 }
