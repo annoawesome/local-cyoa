a01a7c916c244ed3f5df6459f37b744cf4a6dce8
Seth Hoong <hoongseth@gmail.com>
2025-11-27 13:30:54 -0500

    Add topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+function Topbar() {
+  return <nav id="topbar">
+    <input type="file" name="" id="" />
+    <button type="button">Load Adventure</button>
+  </nav>
+}
+
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <Topbar />
     </>
   );
 }
