eb5079c98abe131b57aafb85e9fd025167b21a80
Seth Hoong <hoongseth@gmail.com>
2025-11-27 14:42:19 -0500

    Implement story node traversal

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-import game from "./game.json" with { type: "json" }
+import game from "./game.json" with { type: "json" };
 
 function Topbar() {
-  return <nav id="topbar">
-    <input type="file" name="" id="" />
-    <button type="button">Load Adventure</button>
-  </nav>
+  return (
+    <nav id="topbar">
+      <input type="file" name="" id="" />
+      <button type="button">Load Adventure</button>
+    </nav>
+  );
 }
 
-function BodyText({text}: {text: string}) {
-  return <div id="body-text">
-    <p>{text}</p>
-  </div>
+function BodyText({ text }: { text: string }) {
+  return (
+    <div id="body-text">
+      <p>{text}</p>
+    </div>
+  );
 }
 
 type Choice = {
-  content: string,
-  next: string,
-}
+  content: string;
+  next: string;
+};
 
-function ChoiceList({choices}: {choices: Choice[]}) {
-  const choiceList = choices.map((choice, i) => <button className="btn-choice" key={i}>{choice.content}</button>);
+function ChoiceList({
+  choices,
+  setCurrentNode,
+}: {
+  choices: Choice[];
+  setCurrentNode: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const choiceList = choices.map((choice, i) => (
+    <button
+      className="btn-choice"
+      key={i}
+      onClick={() => {
+        setCurrentNode(choice.next);
+      }}
+    >
+      {choice.content}
+    </button>
+  ));
 
   return choiceList;
 }
 
 export default function App() {
+  const [currentNode, setCurrentNode] = useState("start");
+
   return (
     <>
       <Topbar />
-      <BodyText text={game.content.start.content} />
-      <ChoiceList choices={game.content.start.choices} />
+      <BodyText text={game.content[currentNode].content} />
+      <ChoiceList
+        choices={game.content[currentNode].choices}
+        setCurrentNode={setCurrentNode}
+      />
     </>
   );
 }
